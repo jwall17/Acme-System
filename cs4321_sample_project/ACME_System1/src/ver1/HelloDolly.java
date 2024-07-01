cdbcf45d3520848c4e3ca9fcbc2c0902cd594db9
James Wallace <jwall161716@gmail.com>
2024-06-30 23:33:51 -0400

    Enhancement to the app

--- cs4321_sample_project/ACME_System1/src/ver1/HelloDolly.java
+++ cs4321_sample_project/ACME_System1/src/ver1/HelloDolly.java
@@ -3,7 +3,9 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Person 2 is at work!");
+		
 
 	}
 
